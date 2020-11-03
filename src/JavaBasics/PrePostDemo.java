54ec2a425a0566f2f28b71408ae4b38e4f213fb9
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-03 08:09:43 +0100

    Implements prefix/postfix unary operators

---
+++ src/JavaBasics/PrePostDemo.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+class PrePostDemo {
+    public static void main(String[] args) {
+
+        int i = 3;
+
+        i++;
+        System.out.println(i);                                 // prints 4
+
+        ++i;
+        System.out.println(i);                                 // prints 5
+
+        System.out.println(++i);                              // prints 6
+
+        System.out.println(i++);                              // prints 6
+
+        System.out.println(i);                               // prints 7
+    }
+
+}
