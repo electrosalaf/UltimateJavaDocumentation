5c2b8ce01e823e13c5a769ac58cdadf9f65d39bd
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-03 12:48:45 +0100

    Implements prefix/postfix operators

---
+++ src/JavaBasics/PrePostDemo1.java
@@ -0,0 +1,15 @@
+package JavaBasics;
+
+class PrePostDemo1 {
+    public static void main(String[] args) {
+
+        int i = 3;
+        i++;
+        System.out.println(i);          // prints 4
+        ++i;
+        System.out.println(i);          // prints 5
+        System.out.println(++i);        // prints 6, evaluates to incremented value.
+        System.out.println(i++);        // prints 6, evaluates to original value.
+        System.out.println(i);          // prints 7
+    }
+}
