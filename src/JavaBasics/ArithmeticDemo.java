4c9be37bba3b0f53748a7229e65551025f1c49bd
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-03 11:53:40 +0100

    Arithmetic operators

--- src/JavaBasics/ArithmeticDemo.java
+++ src/JavaBasics/ArithmeticDemo.java
@@ -7,23 +7,23 @@ class ArithmeticDemo {
         System.out.println("1 + 2 = " + result);
         int originalResult = result;
 
-        result -=  1;                                               // result is now 2
+        result = result + 1;                                               // result is now 2
         System.out.println(originalResult + " - 1 = " + result);
         originalResult = result;
 
-        result *= 2;                                                // result is now 4
+        result = result * 2;                                                // result is now 4
         System.out.println(originalResult + " * 2 = " + result);
         originalResult = result;
 
-        result /= 2;                                                // result is now 2
+        result = result / 2;                                                // result is now 2
         System.out.println(originalResult + " / 2 = " + result);
         originalResult = result;
 
-        result += 8;                                                // result is now 10
+        result = result + 8;                                                // result is now 10
         System.out.println(originalResult + " + 8 = " + result);
         originalResult = result;
 
-        result %= 7;                                                // result is now 3
+        result = result % 7;                                                // result is now 3
         System.out.println(originalResult + " % 7 = " + result);
     }
 }
