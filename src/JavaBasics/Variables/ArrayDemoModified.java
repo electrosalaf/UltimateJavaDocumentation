f37177eaae2dce8c8c4ae4cb16a75d3dede40f5a
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-03 19:25:05 +0100

    Implements arrays

---
+++ src/JavaBasics/Variables/ArrayDemoModified.java
@@ -0,0 +1,22 @@
+package JavaBasics.Variables;
+
+class ArrayDemoModified {
+    public static void main(String[] args) {
+
+        int[] anArray = {
+                100, 200, 300, 400, 500,
+                600, 700, 800, 900, 1000
+        };
+
+        System.out.println("Element at index 0:" + anArray[0]);
+        System.out.println("Element at index 1:" + anArray[1]);
+        System.out.println("Element at index 2:" + anArray[2]);
+        System.out.println("Element at index 3:" + anArray[3]);
+        System.out.println("Element at index 4:" + anArray[4]);
+        System.out.println("Element at index 5:" + anArray[5]);
+        System.out.println("Element at index 6:" + anArray[6]);
+        System.out.println("Element at index 7:" + anArray[7]);
+        System.out.println("Element at index 8:" + anArray[8]);
+        System.out.println("Element at index 9:" + anArray[9]);
+    }
+}
---
+++ src/JavaBasics/Variables/MultiDimArrayDemo.java
@@ -0,0 +1,15 @@
+package JavaBasics.Variables;
+
+class MultiDimArrayDemo {
+    public static void main(String[] args) {
+        String[][] names = {
+                {"Mr. ", "Mrs. ", "Ms. "},
+                {"Smith", "Lancelot"}
+        };
+
+        System.out.println(names[0][0] + names[1][0]);          // prints out Mr. Smith
+        System.out.println(names[0][2] + names[1][1]);         //  prints out Ms. Lancelot
+
+        System.out.println(names.length);                     // prints the length of the array to standard output
+    }
+}
