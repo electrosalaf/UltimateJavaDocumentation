537188429b23f9eeec24cb2f1931f0de8728abde
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-04 12:51:27 +0100

    Implements do-while loop

--- src/JavaBasics/ControlFlowStatements/DoWhileDemo.java
+++ src/JavaBasics/ControlFlowStatements/DoWhileDemo.java
@@ -6,4 +6,13 @@
 package JavaBasics.ControlFlowStatements;
 
 class DoWhileDemo {
+    public static void main(String[] args) {
+
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 11);
+    }
 }
+
