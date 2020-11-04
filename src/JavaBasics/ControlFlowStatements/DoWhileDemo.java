63c9f6875ca17ed4bec11b796f3b2bc6fc51c2dc
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-04 12:38:30 +0100

    Implements do-while loop

---
+++ src/JavaBasics/ControlFlowStatements/DoWhileDemo.java
@@ -0,0 +1,9 @@
+/**
+ * The difference between do-while: do-while evaluates its expression at the bottom.
+ * Statements within the do block are always executed at least once.
+ */
+
+package JavaBasics.ControlFlowStatements;
+
+class DoWhileDemo {
+}
