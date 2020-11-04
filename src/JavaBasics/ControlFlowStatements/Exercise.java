c08d1ead29a736bb90b1f7a462c5a84edaabe07c
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-11-04 20:58:11 +0100

    Solution to exercise in the docs

---
+++ src/JavaBasics/ControlFlowStatements/Exercise.java
@@ -0,0 +1,17 @@
+package JavaBasics.ControlFlowStatements;
+
+public class Exercise {
+    public static void main(String[] args) {
+
+        int aNumber = 3;
+
+        if (aNumber >= 0) {
+            if (aNumber == 0) {
+                System.out.print("first string");
+            } else {
+            System.out.println("second string");
+            }
+        }
+        System.out.println("third string");
+    }
+}
