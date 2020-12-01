e6965d29f0a04533417fecdf3e7aa947607f6dd1
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2020-12-01 14:54:51 +0100

    Implements StringBuilder class

--- src/NumbersAndStrings/Strings/StringBuilderDemo.java
+++ src/NumbersAndStrings/Strings/StringBuilderDemo.java
@@ -1,4 +1,13 @@
 package NumbersAndStrings.Strings;
 
 public class StringBuilderDemo {
+    public static void main(String[] args) {
+        String palindrome = "Dot saw I was Tod";
+
+        StringBuilder sb = new StringBuilder(palindrome);
+
+        sb.reverse();
+
+        System.out.println(sb);
+    }
 }
